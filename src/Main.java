cd5394e2070dc29ede879dd43e7972ca17dc4fda
SaraEngsted <sara.andreasen@live.dk>
2022-01-31 11:12:14 +0100

    book finish

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("---Arraylist Exercise---");
+
+
+
+
+        System.out.println("---Book Exercise---");
+
         Biblioteket b = new Biblioteket();
         Book testBook = new Book(1234567,"Kvinde",2020);
         System.out.println(b.doesBibliotekHaveBook(testBook));
